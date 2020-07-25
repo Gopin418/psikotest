8ebd4997f8ffdd6562528819e466fa165c24e801
Alvin Ardiansyah Maulana <alvinardiansyah.18.4@gmail.com>
2020-07-25 21:38:31 +0700

    Finishing Pauli test with axios post

---
+++ src/components/Timer.vue
@@ -0,0 +1,54 @@
+<template lang="html">
+  <div class="">
+    <v-card outlined>
+      <v-card-title class="pb-0">
+        <p class="mx-auto font-weight-thin text-h3">{{ countdown.format('mm:ss') }}</p>
+      </v-card-title>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  data () {
+    return {
+      timeout: '',
+      timeText: '',
+      countdown: moment(15, 'minutes'),
+      count: 0
+    }
+  },
+  created () {
+    this.$root.$refs.timer = this
+  },
+  computed: {
+    counter () {
+      return this.$store.state.timer
+    }
+  },
+  methods: {
+    timeSet () {
+      this.timeText = this.countdown.format('mm:ss')
+      this.timeout = setInterval(() => {
+        this.count += 1
+        this.$store.commit('setTime', this.count)
+        this.countdown = moment(this.countdown).subtract(1, 'seconds')
+        this.timeText = this.countdown.format('mm:ss')
+        if (this.counter === 60) {
+          this.count = 0
+          this.$store.commit('setTime', 0)
+        }
+        if (this.timeText === '00:00') {
+          clearInterval(this.timout)
+          this.countdown = moment(15, 'minutes')
+        }
+      }, 1000)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -8,10 +8,26 @@ import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 import '@babel/polyfill'
 import VueCookies from 'vue-cookies'
+import api from './api.js'
 
 Vue.use(VueCookies)
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = api
+api.defaults.timeout = 10000
+
+api.interceptors.request.use(
+  config => {
+    const token = window.$cookies.get('token')
+    if (token) {
+      config.headers.common.Authorization = token
+    }
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  })
 
 new Vue({
   router,
