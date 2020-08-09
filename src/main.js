b4e8676dc2abe390f84ad4f4fcbad3ee753e6145
Alvin Ardiansyah Maulana <alvinardiansyah.18.4@gmail.com>
2020-08-09 23:52:04 +0700

    update

--- src/main.js
+++ src/main.js
@@ -11,13 +11,9 @@ import axios from 'axios'
 import VueCookies from 'vue-cookies'
 import VueAxios from 'vue-axios'
 
-// import api from './api.js'
-
 Vue.use(VueCookies)
 Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
-
-// Vue.prototype.$http = api
 
 axios.defaults.timeout = 10000
 axios.interceptors.request.use(
@@ -37,18 +33,5 @@ new Vue({
   router,
   store,
   vuetify,
-  mounted () {
-    api.interceptors.request.use(
-      config => {
-        const token = this.$cookies.get('token')
-        if (token) {
-          config.headers.Authorization = token
-        }
-        return config
-      },
-      error => {
-        return Promise.reject(error)
-      })
-  },
   render: h => h(App)
 }).$mount('#app')
