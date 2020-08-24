670d784c2ee9a9544cd9e2ce2f63169bc001b3c1
Syamsu Rizal Ali <syamsu.smansa.polban@gmail.com>
2020-08-24 20:48:15 +0700

    mod 10

--- server/pauli-backend.js
+++ server/pauli-backend.js
@@ -38,8 +38,11 @@ router.post('/simpan-data-jawaban-pauli', function (req, res) {
   var Query1 = ' INSERT INTO t_test (id_user, sesi, tipe_test, nomor_test, waktu) '
   Query1 += ' VALUES (?, ?, ?, ?, ?) '
 
+  // var Query2 = ' INSERT INTO t_jawaban_pauli (id_test, angka_atas, angka_bawah, jawaban, benar_salah) '
+  // Query2 += ' VALUES (?, ?, ?, ?, ?) '
+
   var Query2 = ' INSERT INTO t_jawaban_pauli (id_test, angka_atas, angka_bawah, jawaban, benar_salah) '
-  Query2 += ' VALUES (?, ?, ?, ?, ?) '
+  Query2 += ' VALUES ? '
 
   sql.beginTransaction(function (_err) {
     var data = [idUser, sesi, tipeTest, nomorTest, waktu]
@@ -59,34 +62,61 @@ router.post('/simpan-data-jawaban-pauli', function (req, res) {
 
       console.log(jawaban)
 
+      var dataJawaban = []
       for (let i = 0; i < jawaban.length; i++) {
         var angkaAtas1 = (angkaAtas[i] === undefined) ? null : angkaAtas[i]
         var angkaBawah1 = (angkaBawah[i] === undefined) ? null : angkaBawah[i]
         var jawaban1 = (jawaban[i] === undefined) ? null : jawaban[i]
 
         var benarSalah = ((angkaAtas1 + angkaBawah1) % 10) === jawaban1 ? 1 : 0
+        console.log(((angkaAtas1 + angkaBawah1) % 10) + ' = ' + jawaban1 + ' -> ' + benarSalah)
+        dataJawaban.push([idTest, angkaAtas1, angkaBawah1, jawaban1, benarSalah])
+      }
 
-        data = [idTest, angkaAtas1, angkaBawah1, jawaban1, benarSalah]
-
-        sql.query(Query2, data, function (_err, results, fields) {
+      sql.query(Query2, [dataJawaban], function (_err, results, fields) {
+        if (_err) {
+          pError.kirimPesanError(res, sql, _err, 'Test gagal disimpan, silahkan coba lagi simpan lagi atau hub. admin')
+          return
+        }
+        sql.commit(function (_err) {
           if (_err) {
             pError.kirimPesanError(res, sql, _err, 'Test gagal disimpan, silahkan coba lagi simpan lagi atau hub. admin')
             return
           }
-          if (i >= jawaban.length - 1) {
-            sql.commit(function (_err) {
-              if (_err) {
-                pError.kirimPesanError(res, sql, _err, 'Test gagal disimpan, silahkan coba lagi simpan lagi atau hub. admin')
-                return
-              }
-              console.log('success!')
-              res.send({ succes: 'success' })
-            })
-          }
+          console.log('success!')
+          res.send({ succes: 'success' })
         })
-      }
+      })
     })
   })
 })
 
 module.exports = router
+
+// for (let i = 0; i < jawaban.length; i++) {
+//   var angkaAtas1 = (angkaAtas[i] === undefined) ? null : angkaAtas[i]
+//   var angkaBawah1 = (angkaBawah[i] === undefined) ? null : angkaBawah[i]
+//   var jawaban1 = (jawaban[i] === undefined) ? null : jawaban[i]
+
+//   var benarSalah = ((angkaAtas1 + angkaBawah1) % 10) === jawaban1 ? 1 : 0
+//   console.log(((angkaAtas1 + angkaBawah1) % 10) + ' = ' + jawaban1 + ' -> ' + benarSalah)
+
+//   data = [idTest, angkaAtas1, angkaBawah1, jawaban1, benarSalah]
+
+//   sql.query(Query2, data, function (_err, results, fields) {
+//     if (_err) {
+//       pError.kirimPesanError(res, sql, _err, 'Test gagal disimpan, silahkan coba lagi simpan lagi atau hub. admin')
+//       return
+//     }
+//     if (i >= jawaban.length - 1) {
+//       sql.commit(function (_err) {
+//         if (_err) {
+//           pError.kirimPesanError(res, sql, _err, 'Test gagal disimpan, silahkan coba lagi simpan lagi atau hub. admin')
+//           return
+//         }
+//         console.log('success!')
+//         res.send({ succes: 'success' })
+//       })
+//     }
+//   })
+// }
